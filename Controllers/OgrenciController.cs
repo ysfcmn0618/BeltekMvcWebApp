4bf26d5a1b92c61292614f72c8856305369c0985
yusuf <yusufcimen90@windowslive.com>
2024-06-23 10:32:34 +0300

    Add project files.

---
+++ Controllers/OgrenciController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BeltekMvcWebApp.Controllers
+{
+    public class OgrenciController : Controller
+    {
+        public string Index()
+        {
+            return "Selam canım!!!";
+        }
+
+        public string OgrenciBilgi()
+        {
+            return "Öğrenci Bilgileri";
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BeltekMvcWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
